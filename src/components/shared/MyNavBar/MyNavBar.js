070b3a5c29dfb2923bda2dc16468489bc71acfc3
Emilee Mitchell <emcakes@gmail.com>
2020-01-07 19:21:44 -0600

    navvy

--- src/components/shared/MyNavBar/MyNavBar.js
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 class MyNavBar extends React.Component {
 static propTypes = {
@@ -16,21 +17,39 @@ static propTypes = {
 
     render() {
       const { authed } = this.props;
+      const buildNavBar = () => {
+        // our linter doesn't like if else's
+        if (authed) {
+          return (
+            // you cannot return a list of siblings, you have to return a single element. Essentially the UL is wrapping it
+            <ul className="navbar-nav ml-auth">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Boards</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link" to="/board/new">New Board</Link>
+                </li>
+                <li className="nav-item">
+                <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>
+                </li>
+            </ul>
+          );
+        }
+        return (<ul className="navbar-nav mr-auth"></ul>);
+      };
       return (
-    <div className="MyNavbar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">Pinterest</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-            <div class="my-2 mt-lg-0">
-              {authed && (<button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
-            </div>
-        </div>
-    </nav>
-</div>
+        <div className="MyNavbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand">Pinterest</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {/* function below will check property of auth */}
+            { buildNavBar() }
+          </div>
+        </nav>
+      </div>
       );
     }
 }
