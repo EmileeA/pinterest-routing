269f25b9c234ae4224b749e5fd3f68ed1acb2867
Emilee Mitchell <emcakes@gmail.com>
2020-01-07 19:35:43 -0600

    .

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 // this makes the a tags work (you do the curly braces becaus eyou don't want to import the whole package - just that element)
 import { Link } from 'react-router-dom';
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
 import './Home.scss';
 
 class Home extends React.Component {
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+    // this is setting the state of boards
+      .then((boards) => this.setState({ boards }))
+      .catch((error) => console.error('errpr from get boards', error));
+  }
+
   render() {
     const boardId = '12345';
     return (
